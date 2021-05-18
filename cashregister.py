ffc8a0e7da49fb7ab32152c6f479386564c1d7cd
Jesse LeMaster <jesselmstr@gmail.com>
2021-05-18 02:34:37 -0400

    initial commit

---
+++ cashregister.py
@@ -0,0 +1,30 @@
+first_item = float(input("Enter price of the first item: "))
+second_item = float(input("Enter price of the second item: "))
+card_input = input("Does customer have a club card? (Y/N): ")
+card_bool = None
+if card_input == "Y" or "y":
+    card_bool = True
+elif card_input == "N" or "n":
+    card_bool = False
+else:
+    print("Card input error!")
+tax_rate = float(input("Enter tax rate, e.g. 5.5 for 5.5% tax: ")) * .01
+base_price = first_item + second_item
+print(f'Base price = {round(base_price, 2)}')
+disc_amt = None
+total_price = None
+if first_item >= second_item:
+    second_item = second_item / 2
+else:
+    first_item = first_item / 2
+if card_bool:
+    disc_amt = (first_item + second_item) * .10
+    after_disc = (first_item + second_item) - disc_amt
+    total_price = (after_disc * tax_rate) + after_disc
+    print(f'Price after discounts = {round(after_disc, 2)}')
+    print(f'Total price = {round(total_price, 2)}')
+else:
+    print(f'Price after discounts = {round((first_item + second_item), 2)}')
+    total_price = ((first_item + second_item) * tax_rate) + (first_item + second_item)
+    print(f'Total price = {round(total_price, 2)}')
+
