128167d31f9e87c1c03982fabd7d3f0e68834c6a
Jesse LeMaster <jesselmstr@gmail.com>
2021-05-18 02:41:22 -0400

    added :.2f to lines 13, 24, 25, 27, 29

--- cashregister.py
+++ cashregister.py
@@ -10,7 +10,7 @@ else:
     print("Card input error!")
 tax_rate = float(input("Enter tax rate, e.g. 5.5 for 5.5% tax: ")) * .01
 base_price = first_item + second_item
-print(f'Base price = {round(base_price, 2)}')
+print(f'Base price = {round(base_price, 2):.2f}')
 disc_amt = None
 total_price = None
 if first_item >= second_item:
@@ -21,10 +21,10 @@ if card_bool:
     disc_amt = (first_item + second_item) * .10
     after_disc = (first_item + second_item) - disc_amt
     total_price = (after_disc * tax_rate) + after_disc
-    print(f'Price after discounts = {round(after_disc, 2)}')
-    print(f'Total price = {round(total_price, 2)}')
+    print(f'Price after discounts = {round(after_disc, 2):.2f}')
+    print(f'Total price = {round(total_price, 2):.2f}')
 else:
-    print(f'Price after discounts = {round((first_item + second_item), 2)}')
+    print(f'Price after discounts = {round((first_item + second_item), 2):.2f}')
     total_price = ((first_item + second_item) * tax_rate) + (first_item + second_item)
-    print(f'Total price = {round(total_price, 2)}')
+    print(f'Total price = {round(total_price, 2):.2f}')
 
