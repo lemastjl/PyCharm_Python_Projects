cb297058705b5d4295395958566f0aae4c73a804
Jesse LeMaster <jesselmstr@gmail.com>
2021-05-18 01:45:32 -0400

    initial commit

---
+++ bmimetric2.py
@@ -0,0 +1,16 @@
+kilos = float(input("Please enter weight in kilograms: "))
+meters = float(input("Please enter height in meters: "))
+bmi = kilos/meters**2
+status = None
+if bmi < 18.5:
+    status = "Underweight"
+elif 18.5 <= bmi <= 24.9:
+    status = "Normal"
+elif 25 <= bmi <= 29.9:
+    status = "Overweight"
+elif bmi >= 30:
+    status = "Obese"
+else:
+    status = "Invalid input!"
+
+print(f'BMI is: {bmi}, Status is {status}')
